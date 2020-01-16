193dca3b8f41442a6009701f225030a691119543
Your-Full-Name <jaradamarwa@gmail.com>
2020-01-16 10:10:56 +0200

    Defining variables

---
+++ src/KotlinSyntax.kt
@@ -0,0 +1,22 @@
+/**
+ *
+ * @author Marwa N. Jarada
+ * @date 15.1.2020
+ */
+fun main(args: Array<String>) {
+    //variables
+    var x=5
+    val  pi =3.14 // This variable can'nt be change (conistatnt) if we could change it's value next, we'll usw var rather than val
+    var age=27
+    var sum=x+pi
+    var fname:String="Marwa" // predefined variable, used when we want to define a spesific datatype
+    var lname="Jarada"
+    var concatName=fname+" "+lname
+    println(age)
+    println(sum)
+    println(concatName)
+
+
+
+
+}
